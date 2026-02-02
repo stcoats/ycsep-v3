7ab6e2527f858c3be49894df727b69e8f980d5a8
Steven Coats <coatsste@puhti-login14.bullx>
2026-02-02 14:01:21 +0200

    Change DB download path to /tmp/ for write permissions

--- app/duckdb_utils.py
+++ app/duckdb_utils.py
@@ -3,13 +3,14 @@ import os
 import urllib.request
 
 def get_connection():
-    db_path = "v2_data.db"
+    # Use /tmp/ because it is writable in Rahti pods
+    db_path = "/tmp/v2_data.db"
     db_url = "https://a3s.fi/swift/v1/YCSEP_v2/v2_data.db"
 
-    # Download the ~1GB database if it's not already in the container
+    # Download the ~1GB database if it's not already in the /tmp folder
     if not os.path.exists(db_path):
-        print(f"Downloading DB from {db_url}...")
+        print(f"Downloading DB from {db_url} to {db_path}...")
         urllib.request.urlretrieve(db_url, db_path)
 
-    # Connect to the local copy
+    # Connect to the local copy in /tmp
     return duckdb.connect(db_path, read_only=True)
